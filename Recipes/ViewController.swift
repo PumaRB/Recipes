b9b8c5751a78e6430d773e3172812a5697d8082d
Puma <Pumaredblue@gmail.com>
2023-04-15 13:59:04 +0300

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Rimma Katushina on 15/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
