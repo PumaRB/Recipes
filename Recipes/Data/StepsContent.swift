f99a648b77067d8f9d77a6d364554d48191acd9a
Puma <Pumaredblue@gmail.com>
2023-05-04 00:03:28 +0300

    Ingridients list

--- Recipes/Data/StepsContent.swift
+++ Recipes/Data/StepsContent.swift
@@ -9,7 +9,9 @@ import Foundation
 
 class StepsContent {
     static func getChristmasSaladSteps() -> [Step] {
-        [Step(header: "Step 1", description: "To make the dressing, place the mustards in a large bowl, then gradually whisk in the olive oil, extra virgin olive oil, vinegar and honey until smooth. Crush in the walnuts and add a pinch of black pepper.")
+        [Step(header: " Step 1", description: "To make the dressing, place the mustards in a large bowl, then gradually whisk in the olive oil, extra virgin olive oil, vinegar and honey until smooth. Crush in the walnuts and add a pinch of black pepper."),
+         Step(header: "Step 2", description: "Text."),
+         Step(header: "Step 3", description: "Text.")
         ]
     }
 }
--- Recipes/ViewControllers/RecipeDetailsViewController.swift
+++ Recipes/ViewControllers/RecipeDetailsViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 class RecipeDetailsViewController: UIViewController {
     @IBOutlet var stepCollection: UICollectionView!
     
+    @IBAction func ingridientsButton(_ sender: Any) {
+        performSegue(withIdentifier: "ingridients", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let ingridientsVC = segue.destination as? IngridientsTableViewController else {return}
+        ingridientsVC.recipe = recipe
+//        guard let recipeDetails = segue.destination as? RecipeDetailsViewController else {return}
+////         let cell = sender as! RecipesCollectionViewCell
+//        recipeDetails.recipe = category?.recipes[indexPath.item]
+    }
+    
     var recipe: Recipe?
     
+    @IBOutlet var collectionLayout: UICollectionViewFlowLayout! {
+        didSet {
+                collectionLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+            }
+    }
+    
+    
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -21,6 +42,10 @@ class RecipeDetailsViewController: UIViewController {
   
 extension RecipeDetailsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         recipe?.steps.count ?? 0
     }
 
@@ -28,21 +53,29 @@ extension RecipeDetailsViewController: UICollectionViewDataSource, UICollectionV
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? RecipeDetailsCollectionViewCell else { return UICollectionViewCell () }
         let step = recipe?.steps[indexPath.row]
         cell.stepDescrioptionLabel.text = step?.description
-        cell.stepName.text = "Text"
-        cell.backgroundColor = .black
+        cell.stepName.text = step?.header
         return cell
     }
     
-
+    
     }
     
-    
+//extension RecipeDetailsViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        
+//        let width = collectionView.frame.width - 24
+//        return CGSize(width: width, height: 140)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 24, right: 0)   }
+//}
+
     
 
    
    
     // MARK: - Navigation
-
- 
 
 
