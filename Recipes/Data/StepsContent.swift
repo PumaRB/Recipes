dda09edbed8d40033b2f4b4b5379b07338fd6696
Puma <Pumaredblue@gmail.com>
2023-05-04 23:50:54 +0300

    Table view in recipe details

--- Recipes/Data/StepsContent.swift
+++ Recipes/Data/StepsContent.swift
@@ -9,9 +9,12 @@ import Foundation
 
 class StepsContent {
     static func getChristmasSaladSteps() -> [Step] {
-        [Step(header: " Step 1", description: "To make the dressing, place the mustards in a large bowl, then gradually whisk in the olive oil, extra virgin olive oil, vinegar and honey until smooth. Crush in the walnuts and add a pinch of black pepper."),
-         Step(header: "Step 2", description: "Text."),
-         Step(header: "Step 3", description: "Text.")
+        [Step(header: " Step 1", description: "To make the dressing, place the mustards in a large bowl, then gradually   whisk in the olive oil, extra virgin olive oil, vinegar and honey until smooth. Crush in the walnuts and add a pinch of black pepper."),
+         Step(header: "Step 2", description: "Trim the pear and apple and finely slice into matchsticks, place in the base of the bowl and toss in the dressing."),
+         Step(header: "Step 3", description: "Roughly chop the frisee, trim and finely slice the round red chicory and add to the bowl with the rocket."),
+         Step(header: "Step 4", description: "Trim the long red and white chicory, cut in half, then separate into leaves, finely slice the stalk and add everything to the bowl."),
+         Step(header: "Step 5", description: "Peel the clementines and finely slice horizontally into rounds."),
+         Step(header: "Step 6", description: "Toss everything together and serve.")
         ]
     }
 }
--- Recipes/Views/RecipeDetailsCollectionViewCell.swift
+++ Recipes/Views/RecipeDetailsCollectionViewCell.swift
@@ -9,6 +9,5 @@ import UIKit
 
 class RecipeDetailsCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet var stepName: UILabel!
-    @IBOutlet var stepDescrioptionLabel: UILabel!
+    
 }
